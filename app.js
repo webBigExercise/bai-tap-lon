d624b8bb4fbd8233bbff38a11f1d88a8431c1630
Bui Minh Quang <bmq928@gmail.com>
2018-05-14 10:44:37 +0700

    separate server

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(cors());
+
 app.use('/api', apiRoute);
 
 // catch 404 and forward to error handler
