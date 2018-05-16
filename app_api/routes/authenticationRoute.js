013b4696955e813e6c6bceef8a455f5c9aa1787c
webBigExercise <38399002+webBigExercise@users.noreply.github.com>
2018-05-16 21:48:50 +0700

    delete
    Co-Authored-By: Mai Hoang Nam <namwin98@gmail.com>

--- app_api/routes/authenticationRoute.js
+++
@@ -1,7 +0,0 @@
-const route = require('express').Router();
-const authenticationCtrl = require('../controllers/authenticationCtrl');
-
-
-route.post('/login', authenticationCtrl.login);
-
-module.exports = route;
